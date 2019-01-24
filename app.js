a830fcf4d69383bb8c08d6226d9a536c1d16e36c
Eksa Pramindanata <pramindanata.eksa@gmail.com>
2019-01-24 18:22:56 +0800

    add cors

--- app.js
+++ app.js
@@ -2,14 +2,19 @@ require('dotenv').config();
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const appConfig = require('./config/app');
+const corsConfig = require('./config/cors');
 
 const errorHandler = require('./app/middleware/errorHandler');
 const router = require('./app/controller/index');
 
 const app = express();
 
+app.use(cors({
+  origin: corsConfig.origin,
+}));
 app.use(bodyParser.json());
 app.use(router);
 app.use(errorHandler);
